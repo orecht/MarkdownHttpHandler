c9f43f3cb3287fd2e27ea487b3f738048c106ee6
Oren Recht <oren.recht@pkrtech.com>
2017-01-26 18:03:01 +0000

    Remove stupid test code

--- ORecht.MarkdownHandler/MarkdownHandler.cs
+++ ORecht.MarkdownHandler/MarkdownHandler.cs
@@ -9,12 +9,6 @@ namespace System.Web.Handlers
         // Override the ProcessRequest method.
         public void ProcessRequest(HttpContext context)
         {
-            /*
-            context.Response.Write("<H1>This is an HttpHandler Test.</H1>");
-            context.Response.Write("<p>Your Browser:</p>");
-            context.Response.Write("Type: " + context.Request.Browser.Type + "<br>");
-            context.Response.Write("Version: " + context.Request.Browser.Version);
-            */
             context.Response.Write("<!DOCTYPE html>");
             context.Response.Write("<html>");
             context.Response.Write("<xmp theme=\"united\" style=\"display:none;\">");
